79df82655a4dac09d821f88199331279225db638
jerrycchuang <chuan-chia.huang@ui.com>
2023-03-05 16:24:49 +0800

    fix error

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Dashboard from './src/Dashboard';
+import ReactDOM from 'react-dom';
+import Dashboard from './Dashboard';
+import "./index.css";
+import Home from "./pages/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Dashboard />
-  </React.StrictMode>
-);
+// ReactDOM.render(<Dashboard />, document.getElementById('root'));
+
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Dashboard />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
